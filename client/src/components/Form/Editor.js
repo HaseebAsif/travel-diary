bf7f3f3c8b1829db5dfff0c0ab37bfa52399d495
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-03-29 14:17:13 +0500

    Calender Package Added

--- client/src/components/Form/Editor.js
+++ client/src/components/Form/Editor.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-const EditorComponent = () => {
+const EditorComponent = ({ handleChange }) => {
   return (
     <>
-      <Editor style={{ border: "1px solid black"}} placeholder="Start Writing Your Travel Diary from here ... " />
+      <Editor
+        style={{ border: "1px solid black" }}
+        onChange={handleChange}
+        placeholder="Start Writing Your Travel Diary from here ... "
+      />
     </>
   );
 };
